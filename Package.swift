4cc22f2e831c88188b022a1685bd0d8d1a796a57
Antoine van der Lee <ajvanderlee@gmail.com>
2020-07-07 08:51:19 +0200

    Updated the package

--- Package.swift
+++ Package.swift
@@ -1,20 +1,15 @@
-// swift-tools-version:5.1
-// We're hiding dev, test, and danger dependencies with // dev to make sure they're not fetched by users of this package.
+// swift-tools-version:5.3
 import PackageDescription
 
 let package = Package(
-    name: "Coyote",
-    products: [
-        // dev .library(name: "DangerDeps", type: .dynamic, targets: ["DangerDependencies"]) // dev
+    name: "WeScan",
+    platforms: [
+        .iOS(.v11)
     ],
-    dependencies: [
-        // dev .package(url: "https://github.com/danger/swift", from: "3.0.0"),
-        // dev .package(path: "Submodules/WeTransfer-iOS-CI/Danger-Swift")
+    products: [
+        .library(name: "WeScan", targets: ["WeScan"])
     ],
     targets: [
-        // This is just an arbitrary Swift file in the app, that has
-        // no dependencies outside of Foundation, the dependencies section
-        // ensures that the library for Danger gets build also.
-        // dev .target(name: "DangerDependencies", dependencies: ["Danger", "WeTransferPRLinter"], path: "Submodules/WeTransfer-iOS-CI/Danger-Swift", sources: ["DangerFakeSource.swift"])
+        .target(name: "WeScan", dependencies: []),
     ]
 )
